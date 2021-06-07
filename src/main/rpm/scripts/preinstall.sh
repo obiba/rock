83a8795f806c7f9436d51b9e88e4e1935cf072f4
Yannick Marcon <yannick.marcon@obiba.org>
2021-06-07 12:08:22 +0200

    #2 Make sure service is stopped before install

--- src/main/rpm/scripts/preinstall.sh
+++ src/main/rpm/scripts/preinstall.sh
@@ -7,32 +7,29 @@ $(getent passwd rock >/dev/null)
 if [ $? != 0 ]; then
     useradd -r -g nobody -d /var/lib/rock -s /sbin/nologin \
         -c "User for Rock" rock
-else
+    # set the correct home directory
+    usermod -d /var/lib/rock rock
+    # set the group
+    usermod -g nobody rock
+fi
 
-  # stop the service if running
-  if [ $(systemctl list-unit-files "rock.service" | wc -l) -gt 3 ]; then
-	  systemctl stop rock
-  elif service rock status > /dev/null; then
-    if which service >/dev/null 2>&1; then
-      service rock stop
-    elif which invoke-rc.d >/dev/null 2>&1; then
-      invoke-rc.d rock stop
-    else
-      /etc/init.d/rock stop
-    fi
+# stop the service if running
+if [ $(systemctl list-unit-files "rock.service" | wc -l) -gt 3 ]; then
+  systemctl stop rock
+elif service rock status > /dev/null; then
+  if which service >/dev/null 2>&1; then
+    service rock stop
+  elif which invoke-rc.d >/dev/null 2>&1; then
+    invoke-rc.d rock stop
+  else
+    /etc/init.d/rock stop
   fi
+fi
 
-  # clean old init
-  if [ -e /etc/init.d/rock ]; then
-    chkconfig --del rock
-    rm -f /etc/init.d/rock
-  fi
-
-  # set the correct home directory
-  usermod -d /var/lib/rock rock
-
-  # set the group
-  usermod -g nobody rock
+# clean old init
+if [ -e /etc/init.d/rock ]; then
+  chkconfig --del rock
+  rm -f /etc/init.d/rock
 fi
 
 exit 0

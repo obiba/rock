1b3f661e48e5ab04625398defb4415162bbaab8b
Yannick Marcon <yannick.marcon@obiba.org>
2021-06-18 12:30:54 +0200

    Typo

--- src/main/rpm/scripts/pretrans.sh
+++ src/main/rpm/scripts/pretrans.sh
@@ -20,10 +20,3 @@ if [ ! -e "%{_sharedstatedir}/%{name}/logs" ]; then
   ln -s %{_localstatedir}/log/%{name} %{_sharedstatedir}/%{name}/logs
 fi
 exit 0
-
-%pre
-getent group %{name} >/dev/null || groupadd -r %{name}
-getent passwd %{name} >/dev/null || \
-	  useradd -r -g %{name} -d %{_sharedstatedir}/%{name} -s /sbin/nologin \
-	  -C "%{name} service user" %{name}
-exit 0

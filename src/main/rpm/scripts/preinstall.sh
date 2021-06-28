07713229e06b56092644129b5b76a10e757d3d7f
Yannick Marcon <yannick.marcon@obiba.org>
2021-06-28 10:04:25 +0200

    typo

--- src/main/rpm/scripts/preinstall.sh
+++ src/main/rpm/scripts/preinstall.sh
@@ -1,5 +1,4 @@
 getent group rock >/dev/null || groupadd -r rock
 getent passwd rock >/dev/null || \
-	  useradd -r -g rock -d /var/lib/rock -s /sbin/nologin \
-	  -C "rock service user" rock
+	  useradd -r -g rock -d /var/lib/rock -s /sbin/nologin -c "rock service user" rock
 exit 0

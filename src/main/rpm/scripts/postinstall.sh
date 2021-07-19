97bc568f9d60a1852718c0e702646bda1c5c0798
Yannick Marcon <yannick.marcon@obiba.org>
2021-07-19 15:15:04 +0200

    More R packages installed, for being opal-ready

--- src/main/rpm/scripts/postinstall.sh
+++ src/main/rpm/scripts/postinstall.sh
@@ -2,7 +2,17 @@
 # Install RServe via R
 # prepare R packages install location (not managed by RPM)
 mkdir -p /var/lib/rock/R/library
-Rscript -e "install.packages('Rserve', lib = .libPaths()[2], repos = 'http://www.rforge.net/')"
+# Rserve package
+Rscript -e ".libPaths(.Library.site) ; if (!suppressWarnings(require(Rserve, quietly = TRUE))) { install.packages('Rserve', repos = 'https://www.rforge.net/') }"
+# utility packages
+Rscript -e ".libPaths(.Library.site) ; if (!suppressWarnings(require(unixtools, quietly = TRUE))) { install.packages('unixtools', repos = 'https://www.rforge.net/') }"
+Rscript -e ".libPaths(.Library.site) ; if (!suppressWarnings(require(remotes, quietly = TRUE))) { install.packages('remotes', repos = 'https://cloud.r-project.org/') }"
+Rscript -e ".libPaths(.Library.site) ; if (!suppressWarnings(require(BiocManager, quietly = TRUE))) { install.packages('BiocManager', repos = 'https://cloud.r-project.org/') }"
+# additional packages
+Rscript -e ".libPaths(.Library.site) ; if (!suppressWarnings(require(sqldf, quietly = TRUE))) { install.packages('sqldf', repos = 'https://cloud.r-project.org/') }"
+Rscript -e ".libPaths(.Library.site) ; if (!suppressWarnings(require(haven, quietly = TRUE))) { install.packages('haven', repos = 'https://cloud.r-project.org/') }"
+Rscript -e ".libPaths(.Library.site) ; if (!suppressWarnings(require(labelled, quietly = TRUE))) { install.packages('labelled', repos = 'https://cloud.r-project.org/') }"
+
 chown -R rock:rock /var/lib/rock/R
 chmod -R 750 /var/lib/rock/R
 # make symlink

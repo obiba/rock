c5c7382be71346ff042e6c55c4f0615cdd1e429d
Yannick Marcon <yannick.marcon@obiba.org>
2021-10-12 12:23:25 +0200

    Dist folder linkage moved to posttrans scriplet to ensure old package one was removed (#8)

--- src/main/rpm/scripts/posttrans.sh
+++ src/main/rpm/scripts/posttrans.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+rm -f /usr/share/rock
+find /usr/share/rock-* -empty -type d -delete
+ln -s /usr/share/rock-* /usr/share/rock
 # for update from System-V
 systemctl preset rock.service
 systemctl start rock.service

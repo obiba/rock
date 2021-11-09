8a084d2802bb6ec04cd20e68015f485213d07731
Yannick Marcon <yannick.marcon@obiba.org>
2021-11-09 09:33:05 +0100

    obiba/opal#3682 systemctl preset and start removed from RPM scriptlets

--- src/main/rpm/scripts/posttrans.sh
+++ src/main/rpm/scripts/posttrans.sh
@@ -2,7 +2,4 @@
 rm -f /usr/share/rock
 find /usr/share/rock-* -empty -type d -delete
 ln -s /usr/share/rock-* /usr/share/rock
-# for update from System-V
-systemctl preset rock.service
-systemctl start rock.service
 exit 0

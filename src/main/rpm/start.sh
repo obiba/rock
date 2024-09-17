28047f95311b5fe425e9bcf9f3489de764d233d5
Yannick Marcon <yannick.marcon@obiba.org>
2024-09-18 00:04:25 +0200

    fix: updated start scripts

--- src/main/rpm/start.sh
+++ src/main/rpm/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-$JAVA $JAVA_ARGS -cp "${ROCK_HOME}/conf:${ROCK_DIST}/lib/*" -DROCK_HOME=${ROCK_HOME} -DROCK_DIST=${ROCK_DIST} -DROCK_LOG=${ROCK_LOG} org.springframework.boot.loader.JarLauncher $ROCK_ARGS
+$JAVA $JAVA_ARGS -cp "${ROCK_HOME}/conf:${ROCK_DIST}/lib/*" -DROCK_HOME=${ROCK_HOME} -DROCK_DIST=${ROCK_DIST} -DROCK_LOG=${ROCK_LOG} org.springframework.boot.loader.launch.JarLauncher $ROCK_ARGS

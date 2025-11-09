4b1b893b8e93778595372b0670dffac141759637
Yannick Marcon <yannick.marcon@obiba.org>
2025-11-09 17:55:14 +0100

    feat: use exec when calling java from script

--- src/main/rpm/start.sh
+++ src/main/rpm/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-$JAVA $JAVA_ARGS -cp "${ROCK_HOME}/conf:${ROCK_DIST}/lib/*" -DROCK_HOME=${ROCK_HOME} -DROCK_DIST=${ROCK_DIST} -DROCK_LOG=${ROCK_LOG} org.springframework.boot.loader.launch.JarLauncher $ROCK_ARGS
+exec $JAVA $JAVA_ARGS -cp "${ROCK_HOME}/conf:${ROCK_DIST}/lib/*" -DROCK_HOME=${ROCK_HOME} -DROCK_DIST=${ROCK_DIST} -DROCK_LOG=${ROCK_LOG} org.springframework.boot.loader.launch.JarLauncher $ROCK_ARGS
